b6f0e1109cd4f852d4bb0ae4ed7f118cae54807a
= <=>
2022-07-13 12:11:30 -0400

    Added cylerian removal to install script.

--- Install-Agents.bat
+++ Install-Agents.bat
@@ -91,3 +91,12 @@ rmdir %filebeatExtractedPath%
 rmdir %packetbeatExtractedPath%
 rmdir %elasticExtractedPath%
 rmdir %winlogbeatExtractedPath%
+
+
+Rem Remove Cylerian
+wmic product where "name like '%%cylerian%%'" call uninstall /nointeractive
+sc.exe stop cagent
+sc.exe delete cagent
+taskkill /F /IM cagent.exe
+taskkill /F /IM cagent_monitor.exe
+rmdir /S c:\progra~1\cylerian /Q
